2817b955ffd79f977fc378973dc41e1364911ae6
Gbenro Oni <gbensky92@gmail.com>
2019-06-10 23:18:54 +0100

    Code cleanup

--- app/src/androidTest/java/app/com/mobileassignment/ApplicationTest.java
+++ app/src/androidTest/java/app/com/mobileassignment/ApplicationTest.java
@@ -1,23 +1,19 @@
 package app.com.mobileassignment;
 
-import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 import android.widget.ListView;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-
-import android.content.Context;
+import android.widget.TextView;
 
 import androidx.test.InstrumentationRegistry;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
 
-import java.lang.String;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import app.com.mobileassignment.views.MainActivity;
 
@@ -33,13 +29,9 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.StringContains.containsString;
-
-import androidx.test.rule.ActivityTestRule;
-
-import android.widget.TextView;
-
 import static org.junit.Assert.assertTrue;
 
 
@@ -47,28 +39,23 @@ import static org.junit.Assert.assertTrue;
 @LargeTest
 public class ApplicationTest {
 
-    public static final String STRING_TO_BE_TYPED = "Paris";
-    public static final String CHARACTER_TO_BE_TYPED = "b";
-    public static final String NON_EXISTING_CITY_TO_BE_TYPED = "dfjhd";
-
-    private Context context;
+    private static final String STRING_TO_BE_TYPED = "Paris";
+    private static final String CHARACTER_TO_BE_TYPED = "b";
+    private static final String NON_EXISTING_CITY_TO_BE_TYPED = "dfjhd";
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(
             MainActivity.class);
 
     @Before
-
     public void setUp() {
-        context = InstrumentationRegistry.getTargetContext();
+        Context context = InstrumentationRegistry.getTargetContext();
     }
 
     //Test Cases Automated
-
     //Launch Application
     @Test
     public void LaunchApp() throws InterruptedException {
-
         onView(withId(R.id.search)).check(matches(isDisplayed()));
         onView(withId(R.id.citiesList)).check(matches(isDisplayed()));
     }
@@ -76,13 +63,10 @@ public class ApplicationTest {
     //Search with no character
     @Test
     public void searchWithNoCharacter() throws Exception {
-
         onView(withId(R.id.search))
                 .perform(typeText(""), closeSoftKeyboard());
         onView(withId(R.id.search)).perform(click());
-
         Thread.sleep(5000);
-
         onView(withText(containsString("Hoeksken"))).check(matches(isDisplayed()));
     }
 
@@ -93,30 +77,22 @@ public class ApplicationTest {
         onView(withId(R.id.search))
                 .perform(typeText(CHARACTER_TO_BE_TYPED), closeSoftKeyboard());
         onView(withId(R.id.search)).perform(click());
-
         Thread.sleep(7000);
-
         ListView listView = (ListView) activityTestRule.getActivity().findViewById(R.id.citiesList);
         View view = listView.getChildAt(0);
         TextView textView = view.findViewById(R.id.cityName);
-
-
         String search = textView.getText().toString().toLowerCase();
         assertTrue(search.toLowerCase().contains(CHARACTER_TO_BE_TYPED.toLowerCase()));
     }
 
 
-
     //Search for an existing city
     @Test
     public void searchWithExistingCity() throws Exception {
-
         onView(withId(R.id.search))
                 .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
         onView(withId(R.id.search)).perform(click());
-
         Thread.sleep(7000);
-
         ListView listView = (ListView) activityTestRule.getActivity().findViewById(R.id.citiesList);
         int k = listView.getChildCount();
 
@@ -126,19 +102,15 @@ public class ApplicationTest {
             String search = textView.getText().toString().toLowerCase();
             assertTrue(search.toLowerCase().contains(STRING_TO_BE_TYPED.toLowerCase()));
         }
-
     }
 
     //Search for a non-existing city
     @Test
     public void searchWithNonExistingCity() throws Exception {
-
         onView(withId(R.id.search))
                 .perform(typeText(NON_EXISTING_CITY_TO_BE_TYPED), closeSoftKeyboard());
         onView(withId(R.id.search)).perform(click());
-
         Thread.sleep(7000);
-
         onView(withId(R.id.citiesList)).check(matches(not(isDisplayed())));
     }
 
@@ -146,30 +118,20 @@ public class ApplicationTest {
     //Scroll on List of Cities view
     @Test
     public void scrollOnListView() throws InterruptedException {
-
         Thread.sleep(7000);
-
         onView(withId(R.id.citiesList)).perform(swipeUp());
-
         Thread.sleep(7000);
-
         ListView listView = (ListView) activityTestRule.getActivity().findViewById(R.id.citiesList);
-
-        assertTrue(hasMinimumChildCount(listView.getChildCount()-1).matches(listView));
-
+        assertTrue(hasMinimumChildCount(listView.getChildCount() - 1).matches(listView));
     }
 
     //Select a city from List of cities
     @Test
     public void selectCity() throws InterruptedException {
-
         onData(allOf()).inAdapterView(withId(R.id.citiesList)).atPosition(0).perform(click());
-
         Thread.sleep(7000);
-
         onView(withId(R.id.insert_point)).check(matches(isDisplayed()));
         onView(withContentDescription("Google Map")).check(matches(isDisplayed()));
 
     }
-
 }
